5b784f1ef51e6f9a8fb03959d0084b7ac573f40d
architrave <minoruminoru112@gmail.com>
2026-08-28 11:07:20 +0900

    Show creator credits on grid card hover, sourced from YouTube descriptions

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const works = [
-  { title: "エンドロール - クワガタP feat. 重音テト", year: 2026, youtubeId: "fepw6wKU8D8", thumbnail: "images/01.jpg" },
-  { title: "ねむ「当たり前の終止符」Music Video", year: 2026, youtubeId: "XFhPR9i27eQ", thumbnail: "images/02.jpg" },
-  { title: "メタリックセイメイ「夏のカレンダー」Music Video", year: 2026, youtubeId: "nVs1prnygiE", thumbnail: "images/03.jpg" },
-  { title: "「まいっちゃう」MASA×都鳥-TOTORI-×あーきとれーぶ(アトリエプロジェクト)", year: 2024, youtubeId: "QJsTcvAufng", thumbnail: "images/04.jpg" },
-  { title: "宇宙パイロット「トワレ」OFFICIAL MUSIC VIDEO", year: 2026, youtubeId: "Rowmkcclsm0", thumbnail: "images/05.jpg" },
-  { title: "GREEEEN / TO ALL SPORTS LOVERS　アルバムショート動画", year: 2024, youtubeId: "q23YpekQmYQ", thumbnail: "images/06.jpg" },
-  { title: "FMV３クスッギフト　3月配信映像　【ディレクション】", year: 2025, youtubeId: "7v4eX-Q8k34", thumbnail: "images/07.jpg" },
-  { title: "監督したアニメがファミマで見れます！", year: 2024, youtubeId: "4SJyfo-5izU", thumbnail: "images/08.jpg" },
-  { title: "GRE4N BOYZ / 「星の詩」LYRIC VIDEO", year: 2025, youtubeId: "7lqJ-3v1k1A", thumbnail: "images/09.jpg" }
+  { title: "エンドロール - クワガタP feat. 重音テト", year: 2026, youtubeId: "fepw6wKU8D8", thumbnail: "images/01.jpg", credit: "Music & Lyrics: クワガタP / Movie & Illustration: あーきとれーぶ" },
+  { title: "ねむ「当たり前の終止符」Music Video", year: 2026, youtubeId: "XFhPR9i27eQ", thumbnail: "images/02.jpg", credit: "Vocal, Lyrics & Music: ねむ / Arrangement: 河田一真 / Illustration: 中辻作太朗 / Movie & Animation: あーきとれーぶ" },
+  { title: "メタリックセイメイ「夏のカレンダー」Music Video", year: 2026, youtubeId: "nVs1prnygiE", thumbnail: "images/03.jpg", credit: "Music & Lyrics: ユウジロウ / Arranged by: メタリックセイメイ / Illustration & Movie: あーきとれーぶ" },
+  { title: "「まいっちゃう」MASA×都鳥-TOTORI-×あーきとれーぶ(アトリエプロジェクト)", year: 2024, youtubeId: "QJsTcvAufng", thumbnail: "images/04.jpg", credit: "Illustration & Animation: 都鳥-totori- / Movie: あーきとれーぶ / Music: masa" },
+  { title: "宇宙パイロット「トワレ」OFFICIAL MUSIC VIDEO", year: 2026, youtubeId: "Rowmkcclsm0", thumbnail: "images/05.jpg", credit: "Music & Lyric: Chinozo / Vocal: Yoru / Illust & Movie: あーきとれーぶ" },
+  { title: "GREEEEN / TO ALL SPORTS LOVERS　アルバムショート動画", year: 2024, youtubeId: "q23YpekQmYQ", thumbnail: "images/06.jpg", credit: "Movie: あーきとれーぶ" },
+  { title: "FMV３クスッギフト　3月配信映像　【ディレクション】", year: 2025, youtubeId: "7v4eX-Q8k34", thumbnail: "images/07.jpg", credit: "Movie Editing: あーきとれーぶ / Animation: cacaotane" },
+  { title: "監督したアニメがファミマで見れます！", year: 2024, youtubeId: "4SJyfo-5izU", thumbnail: "images/08.jpg", credit: "Movie Editing: あーきとれーぶ / Animation: cacaotane" },
+  { title: "GRE4N BOYZ / 「星の詩」LYRIC VIDEO", year: 2025, youtubeId: "7lqJ-3v1k1A", thumbnail: "images/09.jpg", credit: "Lyric Video: あーきとれーぶ" }
 ];
